b0310a9055204757570689c1f7729debd7a8d4e0
Teng Siong Ong <siong1987@gmail.com>
2014-07-25 01:02:16 -0700

    fix indentation.

--- apns.go
+++ apns.go
@@ -101,164 +101,164 @@ func (n *Notification) WriteTo(wr io.Writer) (int64, error) {
 }
 
 type APNSClient struct {
-	ctx       appengine.Context
-	pem       string
+  ctx       appengine.Context
+  pem       string
   apnsAddr  string            // "gateway.sandbox.push.apple.com"
   port      string            // "2195"
 }
 
 func New(ctx appengine.Context, pem string, apnsAddr string, port string) *APNSClient {
-	return &APNSClient{
-		ctx:      ctx,
-		pem:      pem,
+  return &APNSClient{
+    ctx:      ctx,
+    pem:      pem,
     apnsAddr: apnsAddr,
     port:     port,
-	}
+  }
 }
 
 func (a *APNSClient) Send(n Notification) error {
-	var err error
-	apnsInitSync.Do(func() {
-		notifChannel = make(chan Notification)
-		err = a.openConn()
-	})
-	if err != nil {
-		return err
-	}
+  var err error
+  apnsInitSync.Do(func() {
+    notifChannel = make(chan Notification)
+    err = a.openConn()
+  })
+  if err != nil {
+    return err
+  }
 
-	n.ctx = a.ctx
-	n.finished = make(chan error)
-	notifChannel <- n
+  n.ctx = a.ctx
+  n.finished = make(chan error)
+  notifChannel <- n
 
-	return <-n.finished
+  return <-n.finished
 }
 
 func (a *APNSClient) dial() (*socket.Conn, net.Conn, error) {
   gaeConn, err := socket.Dial(a.ctx, "tcp", a.apnsAddr+ ":" + a.port)
-	if err != nil {
-		return nil, nil, err
-	}
-	certificate, err := LoadPemFile(a.pem)
-	if err != nil {
-		return nil, nil, err
-	}
+  if err != nil {
+    return nil, nil, err
+  }
+  certificate, err := LoadPemFile(a.pem)
+  if err != nil {
+    return nil, nil, err
+  }
 
-	certs := []tls.Certificate{certificate}
-	conf := &tls.Config{
-		Certificates: certs,
-	}
+  certs := []tls.Certificate{certificate}
+  conf := &tls.Config{
+    Certificates: certs,
+  }
 
-	apnsConn := tls.Client(gaeConn, conf)
+  apnsConn := tls.Client(gaeConn, conf)
 
-	return gaeConn, apnsConn, nil
+  return gaeConn, apnsConn, nil
 }
 
 func (a *APNSClient) openConn() error {
-	gaeConn, apnsConn, err := a.dial()
-	if err != nil {
-		return err
-	}
-	go func() {
-		for {
-			select {
-			case n := <-notifChannel:
-				a.ctx.Infof("Sending apns: %#v", n)
-				gaeConn.SetContext(n.ctx)
-				_, err := n.WriteTo(apnsConn)
-				n.finished <- err
-				if err != nil {
-					a.ctx.Infof("APNS error encountered %s, reconnecting", err.Error())
-					apnsConn.Close()
-					gaeConn, apnsConn, err = a.dial()
-					if err != nil {
-						a.ctx.Infof("apns reconnect: %s", err.Error())
-						return
-					}
-				}
-				a.ctx.Infof("Finished sending apns")
-			case <-time.After(time.Minute):
-				log.Println("resetting apns daemon due to inactivity")
-				apnsConn.Close()
-				apnsInitSync = sync.Once{}
-				return
-			}
-		}
-	}()
+  gaeConn, apnsConn, err := a.dial()
+  if err != nil {
+    return err
+  }
+  go func() {
+    for {
+      select {
+      case n := <-notifChannel:
+        a.ctx.Infof("Sending apns: %#v", n)
+        gaeConn.SetContext(n.ctx)
+        _, err := n.WriteTo(apnsConn)
+        n.finished <- err
+        if err != nil {
+          a.ctx.Infof("APNS error encountered %s, reconnecting", err.Error())
+          apnsConn.Close()
+          gaeConn, apnsConn, err = a.dial()
+          if err != nil {
+            a.ctx.Infof("apns reconnect: %s", err.Error())
+            return
+          }
+        }
+        a.ctx.Infof("Finished sending apns")
+      case <-time.After(time.Minute):
+        log.Println("resetting apns daemon due to inactivity")
+        apnsConn.Close()
+        apnsInitSync = sync.Once{}
+        return
+      }
+    }
+  }()
 
-	return nil
+  return nil
 }
 
 // LoadPemFile reads a combined certificate+key pem file into memory.
 func LoadPemFile(pemFile string) (cert tls.Certificate, err error) {
-	pemBlock, err := ioutil.ReadFile(pemFile)
-	if err != nil {
-		return
-	}
-	return LoadPem(pemBlock)
+  pemBlock, err := ioutil.ReadFile(pemFile)
+  if err != nil {
+    return
+  }
+  return LoadPem(pemBlock)
 }
 
 // LoadPem is similar to tls.X509KeyPair found in tls.go except that this
 // function reads all blocks from the same file.
 func LoadPem(pemBlock []byte) (cert tls.Certificate, err error) {
-	var block *pem.Block
-	for {
-		block, pemBlock = pem.Decode(pemBlock)
-		if block == nil {
-			break
-		}
-		if block.Type == "CERTIFICATE" {
-			cert.Certificate = append(cert.Certificate, block.Bytes)
-		} else {
-			break
-		}
-	}
+  var block *pem.Block
+  for {
+    block, pemBlock = pem.Decode(pemBlock)
+    if block == nil {
+      break
+    }
+    if block.Type == "CERTIFICATE" {
+      cert.Certificate = append(cert.Certificate, block.Bytes)
+    } else {
+      break
+    }
+  }
 
-	///////////////////////////////////////////////////////////////////////////
-	// The rest of the code in this function is copied from the tls.X509KeyPair
-	// implementation found at http://golang.org/src/pkg/crypto/tls/tls.go,
-	// with the exception of minor changes (no need to decode the next block).
-	///////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////
+  // The rest of the code in this function is copied from the tls.X509KeyPair
+  // implementation found at http://golang.org/src/pkg/crypto/tls/tls.go,
+  // with the exception of minor changes (no need to decode the next block).
+  ///////////////////////////////////////////////////////////////////////////
 
-	if len(cert.Certificate) == 0 {
-		err = errors.New("crypto/tls: failed to parse certificate PEM data")
-		return
-	}
+  if len(cert.Certificate) == 0 {
+    err = errors.New("crypto/tls: failed to parse certificate PEM data")
+    return
+  }
 
-	if block == nil {
-		err = errors.New("crypto/tls: failed to parse key PEM data")
-		return
-	}
+  if block == nil {
+    err = errors.New("crypto/tls: failed to parse key PEM data")
+    return
+  }
 
-	// OpenSSL 0.9.8 generates PKCS#1 private keys by default, while
-	// OpenSSL 1.0.0 generates PKCS#8 keys. We try both.
-	var key *rsa.PrivateKey
-	if key, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
-		var privKey interface{}
-		if privKey, err = x509.ParsePKCS8PrivateKey(block.Bytes); err != nil {
-			err = errors.New("crypto/tls: failed to parse key: " + err.Error())
-			return
-		}
+  // OpenSSL 0.9.8 generates PKCS#1 private keys by default, while
+  // OpenSSL 1.0.0 generates PKCS#8 keys. We try both.
+  var key *rsa.PrivateKey
+  if key, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
+    var privKey interface{}
+    if privKey, err = x509.ParsePKCS8PrivateKey(block.Bytes); err != nil {
+      err = errors.New("crypto/tls: failed to parse key: " + err.Error())
+      return
+    }
 
-		var ok bool
-		if key, ok = privKey.(*rsa.PrivateKey); !ok {
-			err = errors.New("crypto/tls: found non-RSA private key in PKCS#8 wrapping")
-			return
-		}
-	}
+    var ok bool
+    if key, ok = privKey.(*rsa.PrivateKey); !ok {
+      err = errors.New("crypto/tls: found non-RSA private key in PKCS#8 wrapping")
+      return
+    }
+  }
 
-	cert.PrivateKey = key
+  cert.PrivateKey = key
 
-	// We don't need to parse the public key for TLS, but we so do anyway
-	// to check that it looks sane and matches the private key.
-	x509Cert, err := x509.ParseCertificate(cert.Certificate[0])
-	if err != nil {
-		return
-	}
+  // We don't need to parse the public key for TLS, but we so do anyway
+  // to check that it looks sane and matches the private key.
+  x509Cert, err := x509.ParseCertificate(cert.Certificate[0])
+  if err != nil {
+    return
+  }
 
-	if x509Cert.PublicKeyAlgorithm != x509.RSA || x509Cert.PublicKey.(*rsa.PublicKey).N.Cmp(key.PublicKey.N) != 0 {
-		err = errors.New("crypto/tls: private key does not match public key")
-		return
-	}
+  if x509Cert.PublicKeyAlgorithm != x509.RSA || x509Cert.PublicKey.(*rsa.PublicKey).N.Cmp(key.PublicKey.N) != 0 {
+    err = errors.New("crypto/tls: private key does not match public key")
+    return
+  }
 
-	return
+  return
 }
